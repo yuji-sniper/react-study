fbbd2fe4548c3f852f5fc8402012c9a291eefb56
吉村直人 <ahowateto123@gmail.com>
2022-08-14 03:23:51 +0900

    不要なimportを削除

--- app/src/components/event/EventShow.tsx
+++ app/src/components/event/EventShow.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { Link, Navigate, useParams } from "react-router-dom";
 import { eventActions } from "../../actions/eventActions";
